4fb95297c0211dee0c4697a30111361b63596c77
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-01-21 19:42:10 -0700

    fixed error in connection and updated env

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env
+  ? new Sequelize(process.env)
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
