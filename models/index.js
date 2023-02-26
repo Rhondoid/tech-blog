5c072a8aae4af7591241501ab0354c4a619d6615
Rhondoid <110504360+Rhondoid@users.noreply.github.com>
2023-02-25 18:19:49 -0700

    added logic to models

--- models/index.js
+++ models/index.js
@@ -1,1 +1,14 @@
 // Manages data and business logic.
+const User = require('./User');
+const Post = require('./Post');
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
